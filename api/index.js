69b72a5c0eb6067e7644ec048303675b697e8766
Harsh Bhamare <harshbhamaremit@gmail.com>
2025-11-18 14:33:43 +0530

    env fixes 1

--- api/index.js
+++ api/index.js
@@ -44,6 +44,8 @@ app.use("/api/faculty", facultyRoutes)
 app.use("/api/defaulter/", defaulterRoutes)
 app.use("/api/submissions/", submissionRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+export default app;
